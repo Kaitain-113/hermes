0830120c1d379b444fae1665385b847c2e8a5381
kaetaen <kaetaen@tutanota.com>
2025-06-08 16:25:11 -0300

    feat: get image from clipboard

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import typer
+from PIL import Image, ImageGrab
 
 app = typer.Typer()
 
+
+def get_image_from_clipboard() -> Image.Image | None:
+    image = ImageGrab.grabclipboard()
+
+    if isinstance(image, Image.Image):
+        return image
+
+    return None
+
 @app.command()
-def gettext():
+def get_text_from_image():
     """
     Extract image from clipboard
     """
+
+    image = get_image_from_clipboard()
+    image.show()
+
     print("test")
 
 
-
+if __name__ == "__main__":
+    app()
