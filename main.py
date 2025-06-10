62f6c6953317bc6ca84028cec806739d13ceeac5
kaetaen <kaetaen@tutanota.com>
2025-06-10 04:26:29 -0300

    feat: extract text from image

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
+import os
 import typer
+import pytesseract
+from uuid import uuid4
 from PIL import Image, ImageGrab
 
 app = typer.Typer()
 
 
-def get_image_from_clipboard() -> Image.Image | None:
+def get_image_from_clipboard() -> str:
     image = ImageGrab.grabclipboard()
 
+    # TO-DO multi operational system
     if isinstance(image, Image.Image):
-        return image
+        image_path = f'/tmp/{uuid4()}.png'
+        image.save(image_path)
+        return image_path
 
     return None
 
+def extract_text_from_image(image_path: str) -> str:
+    if not image_path:
+        return None
+
+    img = Image.open(image_path)
+
+    result = pytesseract.image_to_string(img)
+    return result
+
+
+# TODO: command to setup language
 @app.command()
 def get_text_from_image():
-    """
-    Extract image from clipboard
-    """
-
     image = get_image_from_clipboard()
-    image.show()
-
-    print("test")
+    if not image:
+        raise Exception("No image found in clipboard")
+    
+    text = extract_text_from_image(image)
+    if not text:
+        raise Exception("No text found in image")
+    
+    print(text)
 
 
 if __name__ == "__main__":
